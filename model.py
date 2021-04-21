741861a43c569d4d2ec91c3e26db88eec2126ce0
vincentbons <vincent.bons@qbuzz.nl>
2021-04-21 15:31:00 +0200

    Fix indentation.

--- model.py
+++ model.py
@@ -6,15 +6,18 @@ import torch.optim as optim
 
 from noise import noisy
 
+
 def reparameterize(mu, logvar):
-    std = torch.exp(0.5*logvar)
+    std = torch.exp(0.5 * logvar)
     eps = torch.randn_like(std)
     return eps.mul(std).add_(mu)
 
+
 def log_prob(z, mu, logvar):
     var = torch.exp(logvar)
-    logp = - (z-mu)**2 / (2*var) - torch.log(2*np.pi*var) / 2
+    logp = - (z - mu) ** 2 / (2 * var) - torch.log(2 * np.pi * var) / 2
     return logp.sum(dim=1)
+
 
 def loss_kl(mu, logvar):
     return -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp()) / len(mu)
@@ -42,11 +45,11 @@ class DAE(TextModel):
         super().__init__(vocab, args)
         self.drop = nn.Dropout(args.dropout)
         self.E = nn.LSTM(args.dim_emb, args.dim_h, args.nlayers,
-            dropout=args.dropout if args.nlayers > 1 else 0, bidirectional=True)
+                         dropout=args.dropout if args.nlayers > 1 else 0, bidirectional=True)
         self.G = nn.LSTM(args.dim_emb, args.dim_h, args.nlayers,
-            dropout=args.dropout if args.nlayers > 1 else 0)
-        self.h2mu = nn.Linear(args.dim_h*2, args.dim_z)
-        self.h2logvar = nn.Linear(args.dim_h*2, args.dim_z)
+                         dropout=args.dropout if args.nlayers > 1 else 0)
+        self.h2mu = nn.Linear(args.dim_h * 2, args.dim_z)
+        self.h2logvar = nn.Linear(args.dim_h * 2, args.dim_z)
         self.z2emb = nn.Linear(args.dim_z, args.dim_emb)
         self.opt = optim.Adam(self.parameters(), lr=args.lr, betas=(0.5, 0.999))
 
@@ -67,8 +70,8 @@ class DAE(TextModel):
         logits = self.proj(output.view(-1, output.size(-1)))
         return logits.view(output.size(0), output.size(1), -1), hidden
 
-     def generate(self, z, max_len, alg):
-        assert alg in ['greedy' , 'sample' , 'top5']
+    def generate(self, z, max_len, alg):
+        assert alg in ['greedy', 'sample', 'top5']
         sents = []
         input = torch.zeros(1, len(z), dtype=torch.long, device=z.device).fill_(self.vocab.go)
         hidden = None
@@ -80,9 +83,9 @@ class DAE(TextModel):
             elif alg == 'sample':
                 input = torch.multinomial(logits.squeeze(dim=0).exp(), num_samples=1).t()
             elif alg == 'top5':
-                not_top5_indices=logits.topk(logits.shape[-1]-5,dim=2,largest=False).indices
-                logits_exp=logits.exp()
-                logits_exp[:,:,not_top5_indices]=0.
+                not_top5_indices = logits.topk(logits.shape[-1] - 5, dim=2, largest=False).indices
+                logits_exp = logits.exp()
+                logits_exp[:, :, not_top5_indices] = 0.
                 input = torch.multinomial(logits_exp.squeeze(dim=0), num_samples=1).t()
         return torch.cat(sents)
 
@@ -95,7 +98,7 @@ class DAE(TextModel):
 
     def loss_rec(self, logits, targets):
         loss = F.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1),
-            ignore_index=self.vocab.pad, reduction='none').view(targets.size())
+                               ignore_index=self.vocab.pad, reduction='none').view(targets.size())
         return loss.sum(dim=0)
 
     def loss(self, losses):
@@ -109,7 +112,7 @@ class DAE(TextModel):
         self.opt.zero_grad()
         losses['loss'].backward()
         # `clip_grad_norm` helps prevent the exploding gradient problem in RNNs / LSTMs.
-        #nn.utils.clip_grad_norm_(self.parameters(), clip)
+        # nn.utils.clip_grad_norm_(self.parameters(), clip)
         self.opt.step()
 
     def nll_is(self, inputs, targets, m):
@@ -149,7 +152,7 @@ class AAE(DAE):
     def __init__(self, vocab, args):
         super().__init__(vocab, args)
         self.D = nn.Sequential(nn.Linear(args.dim_z, args.dim_d), nn.ReLU(),
-            nn.Linear(args.dim_d, 1), nn.Sigmoid())
+                               nn.Linear(args.dim_d, 1), nn.Sigmoid())
         self.optD = optim.Adam(self.D.parameters(), lr=args.lr, betas=(0.5, 0.999))
 
     def loss_adv(self, z):
@@ -157,13 +160,13 @@ class AAE(DAE):
         zeros = torch.zeros(len(z), 1, device=z.device)
         ones = torch.ones(len(z), 1, device=z.device)
         loss_d = F.binary_cross_entropy(self.D(z.detach()), zeros) + \
-            F.binary_cross_entropy(self.D(zn), ones)
+                 F.binary_cross_entropy(self.D(zn), ones)
         loss_g = F.binary_cross_entropy(self.D(z), ones)
         return loss_d, loss_g
 
     def loss(self, losses):
         return losses['rec'] + self.args.lambda_adv * losses['adv'] + \
-            self.args.lambda_p * losses['|lvar|']
+               self.args.lambda_p * losses['|lvar|']
 
     def autoenc(self, inputs, targets, is_train=False):
         _, logvar, z, logits = self(inputs, is_train)
